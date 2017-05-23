620d4a78ffd6b3d3abe8974a9f323cfefe17b7ed
BohdanMoroz <bohdanmoroz@gmail.com>
2017-05-23 14:36:03 +0300

    v.1.5

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -40,6 +40,7 @@ public class Board {
     JPanel panel = new JPanel();
     public JPanel fillBoard() {
         panel.setLayout(new GridLayout(10,10));
+        panel.setBounds(20, 20, 250, 250);
         for (int i = 0; i < 10; i++)
             for(int j = 0; j < 10; j++) {
                 panel.add(dock.getShip(getIndex(i,j)));
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,22 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(250, 250);
+        frame.setSize(286, 308);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        JLabel lineUp = new JLabel(new ImageIcon("images/line_up.png"));
+        JLabel lineLeft = new JLabel(new ImageIcon("images/line_left.png"));
+        lineUp.setBounds(20,0,250,20);
+        lineLeft.setBounds(0,20,20,250);
+        frame.add(lineUp);
+        frame.add(lineLeft);
 
         Board board = new Board();
         frame.add(board.fillBoard());
