5c1bad3628856c4a8dff1de7a5164c923c45d20a
VadimMoroz <VadimMoroz@users.noreply.github.com>
2017-05-29 22:52:18 +0200

    Review

--- src/com/company/computer/EnemyBoard.java
+++ src/com/company/computer/EnemyBoard.java
@@ -5,22 +5,22 @@ import java.awt.*;
 
 //think about interface or super class
 public class EnemyBoard {
-    private JPanel panel;
+    private JPanel panel; // [VM:Middle] Should have meaningfull name
     private PlanOfShipPosition planOfShipPosition;
 
     public EnemyBoard(){
-        planOfShipPosition = new PlanOfShipPosition();
+        planOfShipPosition = new PlanOfShipPosition(); // [VM:Middle] Better to init in field declaration
         initPanel();
         fillPanel();
     }
 
     private void initPanel(){
         panel = new JPanel();
-        panel.setLayout(new GridLayout(10,10));
+        panel.setLayout(new GridLayout(10,10));//[VM:High] "10" and "10" should be constant
         panel.setBounds(20, 20, 250, 250);
     }
 
-    private EnemyArea enemyArea = new EnemyArea();
+    private EnemyArea enemyArea = new EnemyArea();// [VM:Middle] All fields should beplaced together
 
     private void fillPanel() {
         for (int i = 0; i < 10; i++)
@@ -33,6 +33,7 @@ public class EnemyBoard {
         return panel;
     }
 
+    // [VM:Middle] Better to change name to "generateIndex" and move to separate class for utils methods.
     private int getIndex(int i, int j) {
         return Integer.parseInt("" + i + j);
     }
