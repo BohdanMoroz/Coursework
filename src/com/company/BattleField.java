7210495369a6d4fc90e972efcbdbd30b2c1c5fb3
BohdanMoroz <bohdanmoroz@gmail.com>
2017-06-08 00:30:46 +0300

    Додав нові класи

--- src/com/company/BattleField.java
+++ src/com/company/BattleField.java
@@ -4,15 +4,11 @@ import java.util.ArrayList;
 
 public class BattleField {
     ButtonBFI[] battleFieldItems = new ButtonBFI[100];
-    ArrayList<Integer>[][] battleFieldSchema = new ArrayList[10][10];
+//    ArrayList<Integer>[][] battleFieldSchema = new ArrayList[10][10];
 
-    public void initBattleFieldSchema() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                battleFieldSchema[i][j].add(0);
-            }
-
-        }
+    public void initBattleFieldItems() {
+        for (int i = 0; i < 100; i++)
+            battleFieldItems[i] = new ButtonBFI();
     }
 
     public void handleShoot() {
